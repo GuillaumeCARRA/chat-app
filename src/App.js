2edc808f4841381af4093a539629dc6990947f53
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-05 16:01:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Home from "../src/components/Home";
+import Login from "../src/components/Login";
+import Register from "../src/components/Register";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Login />
+      <Register />
     </div>
   );
 }

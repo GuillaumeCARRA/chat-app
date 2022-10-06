f43e65a53d932981180e38628f07831f5eacab2e
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-06 10:07:08 +0200

    setting up register component, added css for this component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Home from "../src/components/Home";
-import Login from "../src/components/Login";
+// import Home from "../src/components/Home";
+// import Login from "../src/components/Login";
 import Register from "../src/components/Register";
 
 import './App.css';
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Login />
+      {/* <Home />
+      <Login /> */}
       <Register />
     </div>
   );
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import AddAvatar from '../../assets/images/addAvatar.png';
 
 import './register.css';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='form__container'>
+      <div  className='form__content'>
+        <span className='form__logo'>Chat App</span>
+        <h2 className='form__title'>Créer son compte</h2>
+        <form>
+          <input className= "form__input" type="text"  placeholder='Entrez votre nom'/>
+          <input className= "form__input" type="email" placeholder='email'/>
+          <input className= "form__input" type="password" placeholder='password'/>
+          <input style={{display: 'none'}} className= "form__input" type="file" id="file"/>
+          <label className= "form__label" htmlFor='file'>
+            <img className="form__img" src={AddAvatar} alt="avatar"/>
+            <span>Photo de profil</span>
+          </label>
+          <button className='form__btn'>Créer un profil</button>
+          <p className='form__signin'>Vous possédez déjà un compte ? Connectez-vous</p>
+        </form>
+      </div>
+    </div>
   )
 }
 

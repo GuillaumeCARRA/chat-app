2b6acbfa1aad0845c99d0b667b859d183932dbd6
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-11 12:01:50 +0200

    added new component, added style for Chat and AllMessages component

---
+++ src/components/AllMessages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Message from '../Message/index';
+
+import './allMessages.css';
+
+function AllMessages() {
+  return (
+    <div className='allMessages'>
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+    </div>
+  )
+}
+
+export default AllMessages;
--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import AllMessages from '../AllMessages';
+import Input from '../Input';
+
+import Cam from "../../assets/images/cam.png";
+import Add from "../../assets/images/add.png";
+import More from "../../assets/images/more.png";
 
 import './chat.css';
+
 
 function Chat() {
   return (
     <div className='chat'>
-      Chat
+      <div className='chat__info'>
+        <span>Michelle</span>
+        <div className='chat__icons'>
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <AllMessages />
+      <Input />
     </div>
   )
 }

0165f5393d5df3586c3d3d21a82428b5de3fcb6a
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-12 09:37:46 +0200

    added styles for allMessages and input components

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import Img from "../../assets/images/img.png"
+import Attach from "../../assets/images/attach.png"
+
 import './input.css';
 
 function Input() {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Entrez votre message ici'/>
+      <div className='input__send'>
+        <img src={Attach} alt="piece jointe"/>
+        <input type="file" style={{display: "none"}} id="file"/>
+        <label htmlFor='file'>
+          <img src={Img} alt="piece jointe"/>
+        </label>
+        <button>Envoyer</button>
+      </div>
+    </div>
   )
 }
 

23e50fc8db889b47905622846305d59f8fa9d75c
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-12 10:24:01 +0200

    added css style for all components

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,7 +4,16 @@ import './message.css';
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='message__info'>
+        <img src="https://images.pexels.com/photos/12390070/pexels-photo-12390070.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="fleurs"/>
+        <span>à l'instant</span>
+      </div>
+      <div className='message__content'>
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/12390070/pexels-photo-12390070.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="fleurs" />
+      </div>
+    </div>
   )
 }
 

e5c26b070efda80c86accb5a1f8dfe7fb4ff0b32
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-20 19:30:34 +0200

    setting up react-router-dom and adding routes for components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-// import Home from "../src/components/Home";
-// import Login from "../src/components/Login";
+import Home from "../src/components/Home";
+import Login from "../src/components/Login";
 import Register from "../src/components/Register";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"; 
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      {/* <Login />  */}
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/">
+            <Route index element={<Home />}/>
+            <Route path="login" element={<Login />}/>
+            <Route path="register" element={<Register />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

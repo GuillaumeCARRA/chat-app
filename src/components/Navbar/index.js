9e9d1d4ccd98a29d3bbaeeb4ad4316c800b83387
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-22 16:26:02 +0200

    possibility to found an user in the chat navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
-import img from '../../assets/images/img.png';
+import { AuthContext } from '../../context/AuthContext';
+
+// import img from '../../assets/images/img.png';
 
 import './navbar.css'; 
 
 
+
 function Navbar() {
+
+  const {currUser} = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <span className='navbar__logo'>Tchat</span>
       <div className='navbar__user'>
-        <img className='navbar__img' src={img} alt=""/>
-        <span className='navbar__name'>Joris</span>
+        <img className='navbar__img' src={currUser.photoURL} alt=""/>
+        <span className='navbar__name'>{currUser.displayName}</span>
         <button 
           className='navbar__btn'
           onClick={() => signOut(auth)}
--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+// Create a reference to the cities collection
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from "../../firebase"; 
 
 import './searchBar.css';
 
 function Searchbar() {
+
+  // for the input search
+  const [username, setUsername] = useState("");
+
+  // actual user
+  const [user, setUser] = useState(null);
+  
+  const [error, setError] = useState(false);
+
+  // search an user
+  const handleSearchUser = async () => {
+    const q = query(
+      collection(db, "users"), 
+      where("displayName", "==" , username)
+    );
+
+    try{
+       // function get to retrieve result ==> users
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc contains our user
+        setUser(doc.data())
+      });
+    } catch(error) {
+      setError(true);
+    }
+
+  };
+
+  // listen for ou keyboard action
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearchUser();
+  };
+
   return (
     <div className='search'>
       <div className='search__form'> 
-        <input type="text" placeholder='Trouver un utilisateur'/>
+        <input 
+          type="text" 
+          placeholder='Trouver un utilisateur' 
+          onKeyDown={handleKey} 
+          onChange={e => setUsername(e.target.value)}
+        />
       </div>
-      <div className='user__chat'>
-        <img src="https://images.pexels.com/photos/13885383/pexels-photo-13885383.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""/>
+      {error && <span>Utilisateur introuvable</span>}
+      {user && <div className='user__chat'>
+        <img src={user.photoURL} alt=""/>
         <div className='user__info'>
-          <span className='user__name'>Michelle</span>
+          <span className='user__name'>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   )
 }

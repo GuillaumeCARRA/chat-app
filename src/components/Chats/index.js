3797372305d7f6ea4104dd8a040ddc05af45f530
GuillaumeCARRA <carraguillaume@gmail.com>
2022-10-26 18:24:28 +0200

    setting up function to fetch chat conversations

--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { doc, onSnapshot } from "firebase/firestore";
+import {db} from "../../firebase"; 
 
 import './chats.css';
 
+
 function Chats() {
+
+  // fecth conversation
+  const [chats, setChats] = useState([]); 
+
+  const {currUser} = useContext(AuthContext);
+
+  // we listen in realtime the app when there is some changes
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currUser.id), (doc) => {
+        setChats(doc.data())
+      });
+
+      return () => {
+        unsub()
+      };
+    };
+    currUser.id && getChats()
+  }, [currUser.id])
+
+  console.log(Object.entries(chats));
+
   return (
     <div className='chats'>
       <div className='user__chat'>
